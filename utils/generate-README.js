8340b8cbc475e8869304106a3c38c44457d6e6a6
Cameron Wright <cam.mac.wright@gmail.com>
2021-01-29 18:44:53 -0800

    continuing onr readme

--- utils/generate-README.js
+++ utils/generate-README.js
@@ -2,7 +2,7 @@ function readmePopulation(answers) {
     // ${ varLicense === "MIT License" ? "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)" : answers.varLicense === "Apache License" ? "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)" : answers.varLicense === "Mozilla Public License 2.0" ? "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)" : "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)" }
 
 
-    var license = ""
+    let license = ""
     switch (answers.varLicense) {
         case "Apache License":
             license = "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)"
